6ac2ae9d838440348f6c83a4d64ac5d23456ee5d
raptoravis <raptoravis@qq.com>
2021-06-08 22:05:07 +0800

    ue4dir configure

--- vars.bat
+++ vars.bat
@@ -8,7 +8,24 @@ set PROJECT_DIR=%ROOTDIR%
 set UPROJECT_PATH=%PROJECT_DIR%\%PROJECT%.uproject
 
 set UE4_DIR=D:\dev\Epic Games\UE_4.26
-rem set UE4_DIR=E:\Epic Games\UE_4.25
 set UE4EDITOR_EXE=%UE4_DIR%\Engine\Binaries\Win64\UE4Editor.exe
+
+IF  NOT EXIST %UE4EDITOR_EXE% (
+    set UE4_DIR=D:\EpicGames\UE_4.26
+    set UE4EDITOR_EXE=%UE4_DIR%\Engine\Binaries\Win64\UE4Editor.exe
+
+    IF  NOT EXIST %UE4EDITOR_EXE% (
+        set UE4_DIR=E:\dev\ue4.my.git
+        set UE4EDITOR_EXE=%UE4_DIR%\Engine\Binaries\Win64\UE4Editor.exe
+
+        IF  NOT EXIST %UE4EDITOR_EXE% (
+            set UE4_DIR=D:\dev\ue4.my.git
+            set UE4EDITOR_EXE=%UE4_DIR%\Engine\Binaries\Win64\UE4Editor.exe
+        )
+    )
+)
+
+echo %UE4_DIR%
+
 set UE4EDITOR_CMD_EXE=%UE4_DIR%\Engine\Binaries\Win64\UE4Editor-Cmd.exe
 set BUILD_BAT=%UE4_DIR%\Engine\Build\BatchFiles\Build.bat
