b5af0e458d23c0d56e6e915875765ceaaade399b
liyonggang <liyonggang@ztgame.com>
2021-06-08 14:50:35 +0800

    update bat

--- mp.bat
+++ mp.bat
@@ -10,3 +10,7 @@ timeout /t 3
 set CLIENT_CMD="%UE4EDITOR_EXE%" "%UPROJECT_PATH%" 127.0.0.1 -game -log -consolex=0 -consoley=500 -windowed -resx=960 -resy=540 -winx=640 -winy=540
 echo %CLIENT_CMD%
 start "" %CLIENT_CMD%
+
+set CLIENT_CMD="%UE4EDITOR_EXE%" "%UPROJECT_PATH%" 127.0.0.1 -game -log -consolex=0 -consoley=500 -windowed -resx=960 -resy=540 -winx=640 -winy=540
+echo %CLIENT_CMD%
+start "" %CLIENT_CMD%
--- vars.bat
+++ vars.bat
@@ -7,7 +7,8 @@ set PROJECT=Repsi
 set PROJECT_DIR=%ROOTDIR%
 set UPROJECT_PATH=%PROJECT_DIR%\%PROJECT%.uproject
 
-set UE4_DIR=E:\Epic Games\UE_4.25
+set UE4_DIR=D:\dev\Epic Games\UE_4.26
+rem set UE4_DIR=E:\Epic Games\UE_4.25
 set UE4EDITOR_EXE=%UE4_DIR%\Engine\Binaries\Win64\UE4Editor.exe
 set UE4EDITOR_CMD_EXE=%UE4_DIR%\Engine\Binaries\Win64\UE4Editor-Cmd.exe
 set BUILD_BAT=%UE4_DIR%\Engine\Build\BatchFiles\Build.bat
