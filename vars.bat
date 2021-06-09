3b131e2526596cf62e6ed790040a716d794bc2ab
liyonggang <liyonggang@ztgame.com>
2021-06-09 10:08:41 +0800

    update vars.bat

--- vars.bat
+++ vars.bat
@@ -10,16 +10,16 @@ set UPROJECT_PATH=%PROJECT_DIR%\%PROJECT%.uproject
 set UE4_DIR=D:\dev\Epic Games\UE_4.26
 set UE4EDITOR_EXE=%UE4_DIR%\Engine\Binaries\Win64\UE4Editor.exe
 
-IF  NOT EXIST %UE4EDITOR_EXE% (
+IF  NOT EXIST "%UE4EDITOR_EXE%" (
     set UE4_DIR=D:\EpicGames\UE_4.26
     set UE4EDITOR_EXE=%UE4_DIR%\Engine\Binaries\Win64\UE4Editor.exe
 
-    IF  NOT EXIST %UE4EDITOR_EXE% (
+    IF  NOT EXIST "%UE4EDITOR_EXE%" (
         set UE4_DIR=E:\dev\ue4.my.git
         set UE4EDITOR_EXE=%UE4_DIR%\Engine\Binaries\Win64\UE4Editor.exe
 
-        IF  NOT EXIST %UE4EDITOR_EXE% (
-            set UE4_DIR=D:\dev\ue4.my.git
+        IF  NOT EXIST "%UE4EDITOR_EXE%" (
+            set UE4_DIR=D:\dev\ue4.git
             set UE4EDITOR_EXE=%UE4_DIR%\Engine\Binaries\Win64\UE4Editor.exe
         )
     )
